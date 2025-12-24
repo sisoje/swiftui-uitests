fc6189af2814f9d2e449ebc35ac6d970ef06608c
Lazar Otasevic <>
2025-12-25 02:44:59 +0400

    fix action

--- run.sh
+++ run.sh
@@ -4,27 +4,12 @@ set -e
 echo "📦 Generating project..."
 swift run xcodegen
 
-# List all available iPhone simulators, pick the first one that's available
-UDID=$(xcrun simctl list devices available |
-       grep -w "iPhone" | # filter only iPhone lines
-       grep -Eo "[0-9A-Fa-f-]{36}" | # extract UUID
-       head -1)
-
-if [ -z "$UDID" ]; then
-  echo "❌ No available iPhone simulator found"
-  exit 1
-fi
-
-echo "📱 Using simulator: $UDID"
-
 echo "🧪 Running UI tests..."
 rm -rf TestResults.xcresult
 xcodebuild test \
   -project TestApp.xcodeproj \
   -scheme TestApp \
-  -destination id="$UDID" \
+  -destination 'platform=iOS Simulator,name=Any iOS Device' \
   -enableCodeCoverage YES \
   -derivedDataPath .derivedData \
   -resultBundlePath TestResults.xcresult
-
- 
